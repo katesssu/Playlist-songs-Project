0ebf92e062c33157a2c1de09db9c7d2ef87328d0
katesssu <katiesugrue18@gmail.com>
2024-12-08 22:03:21 +0000

    Attempting at Klint

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,10 @@ plugins {
     // Plugin for Dokka - KDoc generating tool
     id("org.jetbrains.dokka") version "1.9.20"
     application
+    // Add the Ktlint plugin
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
 }
+
 group = "ie.setu"
 version = "1.0-SNAPSHOT"
 
@@ -13,19 +16,23 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // dependencies for logging
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+
+    // For generating a Dokka Site from KDoc
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-    // dependencies for logging
-    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
-    implementation("org.slf4j:slf4j-simple:2.0.16")
-
-
-    // For generating a Dokka Site from KDoc
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
+ktlint {
+    version.set("0.47.1")
+    android.set(false)
+    outputToConsole.set(true)
+    ignoreFailures.set(false)
+    enableExperimentalRules.set(true)
 }
+

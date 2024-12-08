c521cd247dc85433c367591330a2513dcd5e3861
katesssu <katiesugrue18@gmail.com>
2024-12-08 20:24:16 +0000

    adding in dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     kotlin("jvm") version "2.0.10"
+    // Plugin for Dokka - KDoc generating tool
+    id("org.jetbrains.dokka") version "1.9.20"
+    application
 }
-
 group = "ie.setu"
 version = "1.0-SNAPSHOT"
 
@@ -22,4 +24,8 @@ dependencies {
     // dependencies for logging
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
     implementation("org.slf4j:slf4j-simple:2.0.16")
+
+
+    // For generating a Dokka Site from KDoc
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
 }

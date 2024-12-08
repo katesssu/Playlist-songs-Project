275b5f38bae9e23f899183636ce68eb78be83ef4
katesssu <katiesugrue18@gmail.com>
2024-12-08 22:14:22 +0000

    final commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,6 @@ plugins {
     // Plugin for Dokka - KDoc generating tool
     id("org.jetbrains.dokka") version "1.9.20"
     application
-    // Add the Ktlint plugin
-    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
 }
 
 group = "ie.setu"
@@ -28,11 +26,5 @@ tasks.test {
     useJUnitPlatform()
 }
 
-ktlint {
-    version.set("0.47.1")
-    android.set(false)
-    outputToConsole.set(true)
-    ignoreFailures.set(false)
-    enableExperimentalRules.set(true)
-}
+
 
